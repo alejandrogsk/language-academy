f79d3cae08d41d811f2cace11924d5ac74ababb7
alejandrogsk <alejandrogsk9900@gmail.com>
2022-03-07 16:30:29 -0300

    form submit logic

--- javascript/form/optionsTabBehavior.js
+++ javascript/form/optionsTabBehavior.js
@@ -1,36 +1,19 @@
-let opcionesContainer = document.getElementById('languages_options_div');
-let opcionesList = opcionesContainer.getElementsByTagName('input')
-let spanList = opcionesContainer.getElementsByTagName('span');
-
-
-let arrayDeInputs=[];
-let arrayDeSpans=[];
-for(let i=0;i<opcionesList.length;i++){
-    if(opcionesList[i].type == 'radio'){
-        console.log(opcionesList[i].value);
-        arrayDeInputs.push(opcionesList[i].value);
-    }
+/**
+ * This function recives a container than has inside multiples div (with an id) and inputs (with a value).
+ * Then add a listerer for each div. If the if value match with some input.value, the input is now checked
+ */
+function clickeableInputDiv(container){
+    const plansContainer = document.getElementById(container);
+    const allTheInputOptionsForPlans = plansContainer.getElementsByTagName('input');
+    const allTheDivOptionsForPlans = plansContainer.getElementsByTagName('div');
+    Array.from(allTheDivOptionsForPlans).forEach(oneDiv => {
+        oneDiv.addEventListener('click', function(){
+            const res = Array.from(allTheInputOptionsForPlans).find(planInput => {
+                return planInput.value == oneDiv.id
+            })
+            if(res!== undefined)res.checked = true;
+        })
+    });
 }
-
-
-for(let i=0;i<spanList.length;i++){
-    console.log('spanList', spanList[i].id)
-
-    spanList[i].addEventListener('click', function(){
-        let result = arrayDeInputs.find(element => element == spanList[i].id);
-
-        if(result !== undefined){
-            opcionesList.item
-
-        }
-    })
-}
-
-
-function optionBehavior(){
-let caca = 'Inglés'
-let popi ='ingles'
-
-        console.log(caca, popi)
-}
-optionBehavior()
+clickeableInputDiv('languages-options-div');
+clickeableInputDiv('plans-options-div');
